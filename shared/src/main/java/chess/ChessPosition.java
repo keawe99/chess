5dc55395e03581e6fb837110cf7772ce4be4153a
Steven Armstrong <keawe1999@gmail.com>
2025-06-26 19:37:03 -0600

    configured ChessPosition.java

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,14 +1,16 @@
 package chess;
+import java.util.Objects;
 
 /**
  * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int row;
+    private final int column;
 
-    public ChessPosition(int row, int col) {
+    public ChessPosition(int row, int column) {
+        this.row = row;
+        this.column = column;
     }
 
     /**
@@ -16,14 +18,32 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
      * @return which column this position is in
-     * 1 codes for the left row
+     * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // same reference
+        if (o == null || getClass() != o.getClass()) return false; // null or different class
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && column == that.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + ", " + column + ")";
     }
 }
