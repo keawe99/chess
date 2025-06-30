f90dc4d62733a758caf9cc57fe5e5f7b4311319f
Steven Armstrong <keawe1999@gmail.com>
2025-06-30 14:29:03 -0600

    installed maven and made changes to bishop moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 public class ChessPiece {
@@ -41,8 +43,44 @@ public class ChessPiece {
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        List<ChessMove> moves = new ArrayList<>();
+
+    switch (this.type) {
+        case BISHOP -> {
+            int[][] directions = {
+                    {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+            };
+
+            for (int[] dir : directions) {
+                int row = myPosition.getRow();
+                int col = myPosition.getColumn();
+
+                while (true) {
+                    row += dir[0];
+                    col += dir[1];
+
+                    if (row < 1 || row > 8 || col < 1 || col > 8) break;
+
+                    ChessPosition newPos = new ChessPosition(row, col);
+                    ChessPiece targetPiece = board.getPiece(newPos);
+
+                    if (targetPiece == null) {
+                        moves.add(new ChessMove(myPosition, newPos, null));
+                    } else {
+                        if (targetPiece.getTeamColor() != this.pieceColor) {
+                            moves.add(new ChessMove(myPosition, newPos, null));
+                        }
+                        break; // stop at first non-empty square
+                    }
+                }
+            }
+        }
+
+        default -> throw new UnsupportedOperationException("Move generation not implemented for " + type);
     }
+
+    return moves;
+}
 
     public enum PieceType {
         BISHOP, ROOK, KNIGHT, QUEEN, KING, PAWN
