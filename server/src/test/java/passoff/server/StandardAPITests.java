455157a449ec80c7ed8443d3b0ef8b0da8b412c6
Steven Armstrong <keawe1999@gmail.com>
2025-07-22 11:58:58 -0600

    made changes to StandardAPITests

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -26,16 +26,21 @@ public class StandardAPITests {
     }
 
     @BeforeAll
-    public static void init() throws Exception {
-        server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
+    public static void init() {
+        try {
+            server = new Server();
+            var port = server.run(0);
+            System.out.println("Started test HTTP server on " + port);
 
-        serverFacade = new TestServerFacade("localhost", Integer.toString(port));
-        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
-        newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
-        createRequest = new TestCreateRequest("testGame");
+            serverFacade = new TestServerFacade("localhost", Integer.toString(port));
+            existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
+            newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
+            createRequest = new TestCreateRequest("testGame");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     @BeforeEach
     public void setup() {
